9fb702245ac1fe9fccd413c0023759c31577b7ed
Delphina Bailey Nicols <DelphinaBaileyNicols@gmail.com>
2019-04-28 18:34:20 -0400

    array-methods.js  Final question, uses .map() to return an array of all the companies who are participating for a big display of event sponsors.

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -117,3 +117,8 @@ console.log(runners);
 
 // Problem 3
 // The 5K run support team wants to make a huge display showcasing all of the companies that are participating in the charity event. Create a new array that contains the names of all the companies and console.log it.
+
+const sponsors = runners.map((state)=>{
+     return state.company_name;
+});
+console.log(sponsors);
